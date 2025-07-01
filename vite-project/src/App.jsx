3fd085f1f2668ffb9b8cd37a1988383f123cb97a
StewartSidauruk <220711816@students.uajy.ac.id>
2025-07-01 19:57:08 +0700

    feat: creating card for task

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 
 function App() {
   return (
@@ -21,10 +22,49 @@ function App() {
         </div>
 
         <div className="flex gap-2 mb-6">
-          <input type="text" placeholder="Write your task here..." className="w-80 p-2 rounded bg-gray-800 text-white placeholder-gray-400 outline-none"/>
+          <input
+            type="text"
+            placeholder="Write your task here..."
+            className="w-80 p-2 rounded bg-gray-800 text-white placeholder-gray-400 outline-none"
+          />
           <button className="w-10 bg-lime-600 p-2 rounded hover:bg-lime-700">
             +
           </button>
+        </div>
+
+        <div className="space-y-4 w-[370px]">
+          <div className="flex justify-between items-start bg-[#1f1f1f] p-3 rounded border border-gray-700">
+            <div className="flex items-start gap-2 w-full">
+              <input
+                type="checkbox"
+                className="mt-1 form-checkbox text-lime-500"
+              />
+              <span className="break-words text-white">
+                taks kerjanya berat banget tapi saya tetap lanjut
+                meskipun sudah larut malam dan tidak ada kopi lagi di kosan
+              </span>
+            </div>
+            <div className="flex gap-3 text-gray-400 ml-2 mt-1">
+              <FiEdit className="cursor-pointer hover:text-white" />
+              <FiTrash2 className="cursor-pointer hover:text-red-500" />
+            </div>
+          </div>
+
+          <div className="flex justify-between items-start bg-[#1f1f1f] p-3 rounded border border-gray-700">
+            <div className="flex items-start gap-2 w-full">
+              <input
+                type="checkbox"
+                className="mt-1 form-checkbox text-lime-500"
+              />
+              <span className="break-words text-white">
+                mantap
+              </span>
+            </div>
+            <div className="flex gap-3 text-gray-400 ml-2 mt-1">
+              <FiEdit className="cursor-pointer hover:text-white" />
+              <FiTrash2 className="cursor-pointer hover:text-red-500" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
